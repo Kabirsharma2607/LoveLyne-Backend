c6fb0891901a3cb18b1792ac44ae2cf6d54de9de
Kabirsharma2607 <kabirsharma2607@gmail.com>
2025-03-02 23:18:55 +0530

    removed unused code

--- src/main/java/com/example/backend/service/UserService.java
+++ src/main/java/com/example/backend/service/UserService.java
@@ -3,15 +3,11 @@ package com.example.backend.service;
 import com.example.backend.models.User;
 import com.example.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.MessageDigest;
